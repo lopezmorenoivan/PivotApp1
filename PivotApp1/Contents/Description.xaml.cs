7988b7ade96aaf0cb3d569d01c0ec90d133f7771
Iván López Moreno <lopezmorenoivan@gmail.com>
2013-10-10 16:45:01 +0200

    More improvements in the gui

--- PivotApp1/Contents/Description.xaml.cs
+++ PivotApp1/Contents/Description.xaml.cs
@@ -12,16 +12,32 @@ namespace PivotApp1.Contents
 {
     public partial class Description : PhoneApplicationPage
     {
+        Piece piece = new Piece();
+
         public Description()
         {
             InitializeComponent();
 
-            //LoadPieces();
+            //LoadPiece();
         }
 
-        public void LoadPieces()
+        public void LoadPiece()
         {
-            //things with the database
+            //Image.Source = piece.Image();
+
+            Name.Text = piece.Name;
+            Option1.Text = piece.Option1;
+            Option2.Text = piece.Option2;
+        }
+
+        private void NameUpdate(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
